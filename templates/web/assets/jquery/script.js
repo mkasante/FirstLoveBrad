4fadb2676b905e5a5d1bc205ec7cc8867045a2fe
Taiwo Kareem <taiwo.kareem36@gmail.com>
2016-11-29 01:30:36 +0000

    finished event

--- templates/web/assets/jquery/script.js
+++ templates/web/assets/jquery/script.js
@@ -1,25 +1,50 @@
 $(document).ready(function(){
 
-	$(".alphabet").click(function(){
-		loaddata($(this).attr('id'));
+	$(".alphabet").on('change', function(){
+		loaddata("../_group/", $(this).val());
 	});
 
-	function tt(text, id)
+	$(".membership").on('change', function(){
+		loaddata("../_status/", $(this).val());
+	});
+
+	$(".event_date#btn-date").click(function(){
+		var start_date = document.getElementById("form1-g-start_date").value;
+		var end_date = document.getElementById("form1-g-end_date").value;
+
+		loaddata("../event/_daterange/", start_date + "--" + end_date);
+	});
+
+	$(".event_types").on('change', function(){
+		loaddata("../event/_eventtype/", $(this).val());
+	});
+
+	function tt(text, suburl, id)
 	{
 		 $('#table-data tbody').text("");
 		 $('#table-data tbody').append(text);
 		 $('#data-count').text($("#table-data > tbody > tr").length);
 	}
 
-	function loaddata(id){
+	function loaddata(suburl, id){
 
 		$.ajax({
 			// type: "POST",
-			url: "../_group/" + id, 
+			url: suburl + id, 
 			success: function(result){
-				tt(result, id);
+				tt(result, suburl, id);
     		}
     	});
 	}
-	loaddata("ALL")
+
+	if (window.location.pathname.endsWith("/member/all/")){
+		loaddata("../_group/", "ALL");
+	}
+
+	if (window.location.pathname.endsWith("/event/")){
+		var start_date = document.getElementById("form1-g-start_date").value;
+		var end_date = document.getElementById("form1-g-end_date").value;
+
+		loaddata("../event/_daterange/", start_date + "--" + end_date);
+	}
 });
