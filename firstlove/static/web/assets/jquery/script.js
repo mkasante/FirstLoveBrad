7b1e59db6afc92ac4900d56c8a9158ad494f0141
Taiwo Kareem <taiwo.kareem36@gmail.com>
2016-12-02 00:52:08 +0000

    major update

--- firstlove/static/web/assets/jquery/script.js
+++ firstlove/static/web/assets/jquery/script.js
@@ -65,11 +65,19 @@ $(document).ready(function(){
 		loadinfo("welcome/_newsfeed/", "first-timers");
 		loadinfo("welcome/_newsfeed/", "evangelism");
 
-		loadapi("welcome/__api/member");
-		loadapi("welcome/__api/event");
-		loadapi("welcome/__api/academic-institution");
-		loadapi("welcome/__api/attendance");
+	}
+	else if (window.location.pathname === "/welcome/"){
+		loadinfo("_newsfeed/", "birthdays");
+		loadinfo("_newsfeed/", "first-timers");
+		loadinfo("_newsfeed/", "evangelism");		
+	}
 
+	if (window.location.pathname === "/api/"){
+		loadapi("model/member.json");
+		loadapi("model/event.json");
+		loadapi("model/event-type.json");
+		loadapi("model/academic-institution.json");
+		loadapi("model/attendance.json");
 	}
 
 	function loadapi(url){
