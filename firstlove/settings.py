f869dca9c7bd1490d1e447f99b411839513be25b
Taiwo Kareem <taiwo.kareem36@gmail.com>
2016-12-06 21:36:33 +0000

    changed to use postgre database

--- firstlove/settings.py
+++ firstlove/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
+import os, sys
 from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
