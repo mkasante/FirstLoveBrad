8d06049bcecde1607723d9bbf68360f919bd259f
Taiwo Kareem <taiwo.kareem36@gmail.com>
2016-12-10 21:51:44 +0000

    updates

--- firstlove/static/web/assets/jquery/script.js
+++ firstlove/static/web/assets/jquery/script.js
@@ -94,4 +94,24 @@ $(document).ready(function(){
     		}
     	});
 	}
+
+	$('#myInput').keyup(function() {
+		var input, filter, table, tr, td, i;
+		input = document.getElementById("myInput");
+		filter = input.value.toUpperCase();
+		table = document.getElementById("table-data");
+		tr = table.getElementsByTagName("tr");
+
+		for (i = 0; i < tr.length; i++) {
+			td = tr[i].getElementsByTagName("td")[0];
+				if (td) {
+					if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+						tr[i].style.display = "";
+					} 
+					else {
+						tr[i].style.display = "none";
+					}
+				}
+			}
+		});
 });
