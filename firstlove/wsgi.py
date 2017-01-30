8eefd23e498cbf5c79f311c8c918278682f13801
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-01-30 06:25:35 +0000

    added member data 29-01-2017

--- firstlove/wsgi.py
+++ firstlove/wsgi.py
@@ -11,8 +11,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firstlove.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firstlove.settings")
 # application = get_wsgi_application()
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
